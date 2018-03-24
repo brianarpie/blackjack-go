bcb560977845872ebfb49b2e9698604fa6688c88
brianarpie <brian.arpie@gmail.com>
2018-03-24 18:17:06 -0400

    add Sort and DefaultSort as opts to package Deck New method

--- deck/card_test.go
+++ deck/card_test.go
@@ -1,6 +1,9 @@
 package deck
 
-import "fmt"
+import (
+  "fmt"
+  "testing"
+)
 
 func ExampleCard() {
   fmt.Println(Card{Rank: Ace, Suit: Heart})
@@ -13,4 +16,20 @@ func ExampleCard() {
   // Two of Spades
   // Three of Clubs
   // Four of Diamonds
+}
+
+func TestNew(t *testing.T) {
+  cards := New()
+  // 13 ranks * 4 suits
+  if len(cards) != 13*4 {
+    t.Error("Wrong number of cards in a new deck.")
+  }
+}
+
+func TestDefaultSort(t *testing.T) {
+  cards := New(DefaultSort)
+  exp := Card{Rank: Ace, Suit: Spade}
+  if cards[0] != exp {
+    t.Error("Expected Ace of Spades as first card. Received:", cards[0])
+  }
 }
