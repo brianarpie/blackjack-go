e185a58c30c0517b396dd5ecac150ae4b713eda0
brianarpie <brian.arpie@gmail.com>
2018-03-26 21:57:01 -0400

    deal cards from deck simple example

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "github.com/brianarpie/blackjack-go/deck"
+  "fmt"
+  "strings"
+)
+
+type Hand []deck.Card
+
+func (h Hand) String() string {
+  strs := make([]string, len(h))
+  for i := range h {
+    strs[i] = h[i].String()
+  }
+  return strings.Join(strs, ", ")
+}
+
+func main() {
+  cards := deck.New(deck.Deck(3), deck.Shuffle)
+  var card deck.Card
+  var player, dealer Hand
+  for i := 0; i < 2; i++ {
+    for _, hand := range []*Hand{&player, &dealer} {
+      card, cards = cards[0], cards[1:]
+      *hand = append(*hand, card)
+    }
+  }
+  fmt.Println("Player:", player)
+  fmt.Println("Dealer:", dealer)
+}
